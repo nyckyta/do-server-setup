16c3e8ea823bddd396cd42e99e44d73e4b583b44
Mykyta <mykytas-mac@Mykytas-MacBook-Pro.local>
2023-08-22 01:24:22 +0300

    Add basic setup
    It installs jq, net-tools and docker from the official script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-get update
+
+# utils
+apt-get install jq net-tools
+
+# docker install
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh ./get-docker.sh
+systemctl enable docker.service
+systemctl enable containerd.service
+echo '{"log-driver":"local"}' | cat > /etc/docker/daemon.json
